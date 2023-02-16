e993442d5f28ca8b8aa3100eee5eaaf092d23ff5
RNDr. Simon Toth <business@simontoth.eu>
2023-02-16 18:44:45 +0100

    Cleanup configuration.

--- docs/conf.py
+++ docs/conf.py
@@ -1,22 +1,17 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
-
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-
+# Basic configuration
 project = 'ModernDoc'
 copyright = '2023, Šimon Tóth'
 author = 'Šimon Tóth'
 
-# -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-
+# Extensions to use
 extensions = [ "breathe", "exhale" ]
+
+# Configuration for the breathe extension
+# Which directory to read the Doxygen output from
 breathe_projects = {"ModernDoc":"xml"}
 breathe_default_project = "ModernDoc"
 
+# Configuration for the exhale extensions
 exhale_args = {
     "containmentFolder": "./api",
     "doxygenStripFromPath": "../src",
@@ -24,13 +19,7 @@ exhale_args = {
     "rootFileTitle": "Library API",
 }
 
-templates_path = ['_templates']
-exclude_patterns = []
-
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
-
+# Configuration for the theme
 html_theme = "sphinx_book_theme"
 html_theme_options = {
     "repository_url": "https://github.com/HappyCerberus/modern-documentation",
