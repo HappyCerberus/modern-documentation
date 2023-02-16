2980e3857727c10db4c5fc531dd6a09e5a36bbe1
RNDr. Simon Toth <business@simontoth.eu>
2023-02-16 10:51:39 +0100

    Remove static path.

--- docs/conf.py
+++ docs/conf.py
@@ -25,7 +25,6 @@ exclude_patterns = []
 
 
 html_theme = "sphinx_book_theme"
-html_static_path = ['_static']
 html_theme_options = {
     "repository_url": "https://github.com/HappyCerberus/modern-documentation",
     "use_repository_button": True,
